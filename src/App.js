678540f10dadb0a0d77174fcffa515a72952b2a4
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-10-22 21:26:31 -0400

    implemented api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import Clarifai from 'clarifai';
+import React, {useState} from 'react';
 import Nav from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageForm from './components/ImageForm/ImageForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css';
+
+
+const app = new Clarifai.App({
+  apiKey: '710e4185216f486dbfa1fa62118ae660'
+ });
+
 
 const particleOptions = {
   particles: {
@@ -20,6 +27,24 @@ const particleOptions = {
 }
 
 function App() {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+
+  const onInputChange = e => {
+    setInput(e.target.value)
+  };
+
+  const onSubmit = () => {
+    setImageUrl(input);
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        input)
+      .then(response => console.log(response.outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
       <Particles 
@@ -29,9 +54,10 @@ function App() {
       <Nav/>
       <Logo />
       <Rank/>
-      <ImageForm />
-      {/* 
-      <FaceRecognition/> */}
+      <ImageForm 
+        onInputChange={onInputChange} 
+        onSubmit={onSubmit}/>
+      <FaceRecognition imageUrl={imageUrl}/> 
     </div>
   );
 }
